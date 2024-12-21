7f6ce27afbc2849974af6bf95f84929742bb8485
theMJ5377 <atwilmarkjoseph85@gmail.com>
2024-12-21 00:33:41 -0800

    Add files via upload

---
+++ exercise1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class exercise1 {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner (System.in);
+        
+        //Write a program to print all alphabets from a to z.
+        // using for loop
+        
+        
+            for (char A = 'a'; A <= 'z'; A++ ){
+
+                System.out.println("Alphabets: " + A + " " );
+                
+            }    
+            
+    }
+}
